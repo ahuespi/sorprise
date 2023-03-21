4fab3e52d7b140ad9d8489578fd96468e85a0025
ahuespi <amir.huespi@gmail.com>
2023-03-21 17:20:30 -0300

    first commit

---
+++ tootsie/pages/finish/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="finish__container">
+      <h1>
+        FELICITACIONES! <br />
+        <br />
+        Estas a un paso de descubrir el caso!{" "}
+      </h1>
+      <p>
+        <br />
+        Por favor, ahora agarrá tu celular, escanea este QR y abramos lo que
+        contiene esta caja!
+      </p>
+      <img src="./Untitled.png" width={200}></img>
+    </div>
+  );
+};
+
+export default index;
---
+++ tootsie/pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Bienvenida Caris!</h1>
+      <br />
+      <br />
+      <div className="container">
+        <p>Un gusto saludarte, soy Mr. Sherlock, Sherlock Holmes.</p>
+        <p>
+          Antes de empezar a contarte lo que necesito contarte.. Quiero que
+          abras el siguiente link y puedas sumergirte en esta aventura:{" "}
+          <a
+            target="_blank"
+            href="https://www.youtube.com/watch?v=pgLjYsVP4H0&ab_channel=RelaxationHarmony"
+          >
+            Hace clic aca
+          </a>
+        </p>
+        <p>
+          Si bien, puedo descubrir cualquier tipo de casos, ya que no hay nada
+          imposible para mi, hoy necesito de tu ayuda{" "}
+        </p>
+        <p>
+          Me llegó el rumor, que dentro de tu casa, hay un tesoro. Pero como
+          estoy con covid, no puedo ir a resolverlo. Así que serás la encargada
+          de encontrar lo que estamos buscando.
+        </p>
+        <p>Una vez que termines, hace clic en "Ya encontré todas las pistas"</p>
+        <p>
+          Mi querido Watson me llamó muy desesperado desde birmingham y me dijo:
+          "The first clue is under the table".
+        </p>
+
+        <button className='button_finish'>
+          <a className='button_finish' href="/finish">Ya encontré todas las pistas!</a>
+        </button>
+      </div>
+    </div>
+  )
+}
